9a02461b5c63326e80152a8ee03798a377a5df77
tommymandude <ThomasKimura@Thomass-iMac.local>
2013-10-03 22:54:28 -0400

    Main

---
+++ Chapter 5/Whereami/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Whereami
+//
+//  Created by Thomas Kimura on 2013-09-22.
+//  Copyright (c) 2013 Thomas Kimura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WhereamiAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WhereamiAppDelegate class]));
+    }
+}
