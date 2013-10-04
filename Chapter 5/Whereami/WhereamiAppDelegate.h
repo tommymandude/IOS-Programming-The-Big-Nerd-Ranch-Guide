ddf8d5c2983cf2edd1c4d3a2cb2a8bb190ee6d96
tommymandude <ThomasKimura@Thomass-iMac.local>
2013-10-03 22:53:51 -0400

    Xcode files

---
+++ Chapter 5/Whereami/WhereamiAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  WhereamiAppDelegate.h
+//  Whereami
+//
+//  Created by Thomas Kimura on 2013-09-22.
+//  Copyright (c) 2013 Thomas Kimura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class WhereamiViewController;
+
+@interface WhereamiAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) WhereamiViewController *viewController;
+
+@end
