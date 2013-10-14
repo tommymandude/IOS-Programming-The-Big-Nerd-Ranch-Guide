aa5d21210049e50c6ee4c6f5f21cb59399f97774
tommymandude <ThomasKimura@Thomass-iMac.local>
2013-10-14 18:26:51 -0400

    Xcode files

---
+++ Chapter 6/Hypnosister/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hypnosister
+//
+//  Created by Thomas Kimura on 2013-10-14.
+//  Copyright (c) 2013 Thomas Kimura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HypnosisterAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HypnosisterAppDelegate class]));
+    }
+}
